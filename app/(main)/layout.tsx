67e56785bb0740adbe6e31ca3c455d827de5d680
remus00 <135813551+remus00@users.noreply.github.com>
2024-03-26 11:03:22 +0100

    Learn Page Wrappers

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,7 +7,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
             <MobileHeader />
             <Sidebar className="hidden lg:flex" />
             <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
-                <div className="h-full">{children}</div>
+                <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
             </main>
         </>
     );
