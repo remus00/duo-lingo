c85138c24934123a8b70995290870fc8913f61c9
remus00 <135813551+remus00@users.noreply.github.com>
2024-03-26 09:06:25 +0100

    Landing Page Skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from './footer';
+import { Header } from './header';
+
+interface MarketingLayoutProps {
+    children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: MarketingLayoutProps) => {
+    return (
+        <div className="flex min-h-screen flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col items-center justify-center">{children}</main>
+            <Footer />
+        </div>
+    );
+};
+
+export default MarketingLayout;
