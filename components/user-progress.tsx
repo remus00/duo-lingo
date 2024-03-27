cd23a2614b6db296fc802cffa0e4a7426a2c4876
remus00 <135813551+remus00@users.noreply.github.com>
2024-03-27 16:35:44 +0100

    seed the db with a seed scropt

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,10 +1,11 @@
+import { courses } from '@/db/schema';
 import { InfinityIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from './ui/button';
 
 type Props = {
-    activeCourse: { imageSrc: string; title: string };
+    activeCourse: typeof courses.$inferSelect;
     hearts: number;
     points: number;
     hasActiveSubscription: boolean;
